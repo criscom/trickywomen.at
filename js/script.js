d9da934aa1d568c82f8dd3f2daa38e1f1cf48fb8
Christoph Potzinger <christoph.potzinger@sunlime.at>
2018-06-08 15:39:42 +0200

    TRICKYWOM-115: #comment GA opt-out checkbox

--- js/script.js
+++ js/script.js
@@ -11,17 +11,31 @@
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($) {
-
-/* START: SMARTMENUS */
-    (function($) {
-    // initialise smartmenus for main menu
-    // $('#main-menu').smartmenus();
-    })(jQuery);
+  /* START: SMARTMENUS */
+  (function($) {
+  // initialise smartmenus for main menu
+  // $('#main-menu').smartmenus();
+})(jQuery);
 /* END: SMARTMENUS */
 
-  $(document).ready(function(e) {
-    $('img[usemap]').rwdImageMaps();
-  });
+$(document).ready(function(e) {
+  $('img[usemap]').rwdImageMaps();
+});
+
+// TRICKYWOM-115 DSGVO MAßnahmen umsetzen
+// Google Analytics Opt-out
+jQuery(function() {
+  jQuery('#google-opt-out').on('change', function() {
+    if(jQuery(this).prop('checked')) {
+      var d = new Date();
+      d.setTime(d.getTime() + 5*360*24*60*60*1000);
+      document.cookie = "google-opt-out=1; expires=" + d.toUTCString() + "; path=/;";
+    } else {
+      document.cookie = "google-opt-out=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    }
+  }).prop('checked', document.cookie.indexOf('google-opt-out') != -1);
+});
+
 
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.my_custom_behavior = {
